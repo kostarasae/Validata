b2bce8e9dead5814bbe82e3e09fe20b11457ffa0
kostarasae <32982168+kostarasae@users.noreply.github.com>
2024-04-13 23:18:28 +0100

    Add files via upload

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,38 @@
+﻿using Data.Data;
+using Data.Models.Domain;
+using Data.Repositories.CustomerRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly CustomerOrdersDbContext dbContext;
+        public ICustomerRepository _repo { get; set; }
+
+        public CustomersController(ICustomerRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCustomers()
+        {
+            var customers = dbContext.Customers.ToList();
+            _repo.GetType
+            return Ok(customers);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCustomer(Customer customer)
+        {
+            _repo.Create(customer);
+            _repo.Save();
+            return Ok(customer);
+        }
+    }
+}
---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,56 @@
+﻿using Data.Data;
+using Data.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly CustomerOrdersDbContext dbContext;
+        public OrdersController(CustomerOrdersDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllOrders()
+        {
+            var orders = dbContext.Orders.ToList();
+            return Ok(orders);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetOrderById([FromRoute] Guid id) 
+        {
+            var order = dbContext.Orders.Find(id);
+            if (order == null) 
+            {
+                return NotFound();
+            }
+            return Ok(order);
+         }
+
+        [HttpPost]
+        public IActionResult AddOrder([FromBody] Order addOrderRequest)
+        {
+            var orderDomainModel = new Order
+            {
+                Id = addOrderRequest.Id,
+                Items = addOrderRequest.Items,
+                Date = DateTime.UtcNow,
+                TotalPrice = addOrderRequest.TotalPrice,
+                Customer = addOrderRequest.Customer
+            };
+
+            dbContext.Orders.Add(orderDomainModel);
+            dbContext.SaveChanges();
+
+            return CreatedAtAction(nameof(GetOrderById), new { id = orderDomainModel.Id }, orderDomainModel);
+        }        
+    }
+}
